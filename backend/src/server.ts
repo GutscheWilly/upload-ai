4f9c475dfdd0d1ceec6ff6902f2e4af8a3f880e3
Willy Gutsche <willy.gutsche@unifesp.br>
2023-09-16 17:21:07 -0300

    Route to generate ai completion

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,20 @@
 import { fastify } from 'fastify';
+import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createVideoTranscriptionRoute } from './routes/create-video-transcription';
+import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 
 const app = fastify();
 
+app.register(fastifyCors, {
+    origin: '*'
+});
+
 app.register(getAllPromptsRoute);
-app.register(uploadVideoRoute)
+app.register(uploadVideoRoute);
 app.register(createVideoTranscriptionRoute);
+app.register(generateAiCompletionRoute);
 
 app.listen({
     port: 3333
