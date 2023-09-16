6c162f1537d21077aca591c2e1ed836cdab4a344
Willy Gutsche <willy.gutsche@unifesp.br>
2023-09-16 15:56:36 -0300

    Route to create video transcription

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,13 @@
 import { fastify } from 'fastify';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
+import { createVideoTranscriptionRoute } from './routes/create-video-transcription';
 
 const app = fastify();
 
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute)
+app.register(createVideoTranscriptionRoute);
 
 app.listen({
     port: 3333
